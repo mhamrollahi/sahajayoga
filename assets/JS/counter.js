9c8e76683887b8e95bcad7b5c7d648716b645ae4
mh Amrollahi <mh.amrollahi@gmail.com>
2023-12-02 23:30:47 +0330

    change some html and css and js file , fixed getSelfFreedom.html

---
+++ assets/JS/counter.js
@@ -0,0 +1,65 @@
+//Start : Moving the photo in the counter area
+// ****************************************************************************************
+
+const counterSec1 = document.getElementById("countersec1");
+
+const compSyles = getComputedStyle(counterSec1);
+let posy = compSyles.getPropertyValue("background-position-y");
+
+// console.log('pos y =', posy);
+//باید شماره فایل استایل.سی اس اس رو بدیم ایندکس از صفر شروع می شه..
+const stylesheet = document.styleSheets[6];
+
+// console.log('styleSheets ==>', stylesheet);
+
+const divParaRule = [...stylesheet.cssRules].find(
+  (r) => r.selectorText === "section.counter-section-wrapper"
+);
+
+posy = posy.replace("%", "");
+// console.log('pos y2 =', posy);
+// console.log('divPrarRule == ',divParaRule);
+
+// console.log('document.body.scrollHeight ==', document.body.scrollHeight);
+
+const maxHeight = document.body.scrollHeight - window.innerHeight;
+// console.log('window = ', (window.pageYOffset * 100) / maxHeight);
+
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  // console.log('Scroll = ' + scroll)
+  // console.log('pos y == ', posy);
+  // console.log('window = ', (window.pageYOffset * 100) / maxHeight);
+  posy = Math.floor((window.pageYOffset * 100) / maxHeight);
+  // console.log('pos y after == ', posy);
+  posy -= 40;
+  divParaRule.style.setProperty("background-position-y", posy + "%");
+  // divParaRule.style.setProperty('background-position-y') = "20px";
+});
+
+// ****************************************************************************************
+//End : Moving the photo in the counter area
+
+//Start : Counter for Section 1
+// ****************************************************************************************
+const counters = document.querySelectorAll(".counter-s1");
+
+counters.forEach((counter) => {
+  function updateCount() {
+    const target = +counter.getAttribute("data-count");
+    const count = +counter.innerHTML;
+
+    const inc = Math.floor((target - count) / 100);
+
+    if (count < target && inc > 0) {
+      counter.innerHTML = count + inc;
+
+      setTimeout(updateCount, 300);
+    } else {
+      counter.innerHTML = target;
+    }
+  }
+  updateCount();
+});
+// ****************************************************************************************
+//End : Counter for Section 1
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,81 +1,26 @@
 AOS.init({
   duration: 800,
-  easing: 'slide'
+  easing: "slide",
 });
 
-document.body.style.overflowX= "hidden";
+document.body.style.overflowX = "hidden";
 
-const counterSec1 = document.getElementById('countersec1');
+// Start -- Scroll to Top Section
+// ****************************************************************************************
 
-const compSyles = getComputedStyle(counterSec1);
-let posy = compSyles.getPropertyValue('background-position-y');
-
-// console.log('pos y =', posy);
-//باید شماره فایل استایل.سی اس اس رو بدیم ایندکس از صفر شروع می شه..
-const stylesheet = document.styleSheets[6];
-
-// console.log('styleSheets ==>', stylesheet);
-
-const divParaRule = [...stylesheet.cssRules].find(
-  (r) => r.selectorText === "section.counter-section-wrapper"
-);
-
-posy = posy.replace('%', '');
-// console.log('pos y2 =', posy);
-// console.log('divPrarRule == ',divParaRule);
-
-// console.log('document.body.scrollHeight ==', document.body.scrollHeight);
-
-const maxHeight = document.body.scrollHeight - window.innerHeight;
-// console.log('window = ', (window.pageYOffset * 100) / maxHeight);
-
-
-window.addEventListener("scroll", (event) => {
-  let scroll = this.scrollY;
-  // console.log('Scroll = ' + scroll)
-  // console.log('pos y == ', posy);
-  // console.log('window = ', (window.pageYOffset * 100) / maxHeight);
-  posy = Math.floor((window.pageYOffset * 100) / maxHeight);
-  // console.log('pos y after == ', posy);
-  posy -= 40;
-  divParaRule.style.setProperty('background-position-y', posy + '%')
-  // divParaRule.style.setProperty('background-position-y') = "20px";
-});
-
-//-------Start:Counter for Section 1
-//-----------------------------------------------------------------
-const counters = document.querySelectorAll('.counter-s1');
-
-counters.forEach(counter => {
-  function updateCount(){
-    const target = +counter.getAttribute('data-count');
-    const count = +counter.innerHTML;
-
-    const inc = Math.floor((target - count )/100);
-
-    if(count < target && inc > 0){
-      counter.innerHTML = (count + inc);
-
-      setTimeout(updateCount,300)
-    }else{
-      counter.innerHTML  = target;
-    }
-  }
-  updateCount();
-})
-//-----------------------------------------------------------------
-//-------End:Counter for Section 1
-
-$(window).scroll(function(){
-  if($(this).scrollTop() >=350){
-      $('#scroll-top').fadeIn();
-  }else{
-      $('#scroll-top').fadeOut();
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 350) {
+    $("#scroll-top").fadeIn();
+  } else {
+    $("#scroll-top").fadeOut();
   }
 });
-$('#scroll-top').click(function(){
-  $('html, body').animate({scrollTop:0}, 'slow');
-    return false;
+$("#scroll-top").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
 
   // $('body,html').animate({scrollTop:0},1500)
 });
+
+// ****************************************************************************************
+// End -- Scroll to Top Section
