d5078715f6d8e6b020b8d58d22189e42b2bbbda3
Mohammad Hassan Amrollahi <mh.amrollahi@gmail.com>
2024-03-14 16:22:13 +0330

    add save experience to index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import fs from 'fs/promises'
 const app = express()
 const port = 3000
 const router = express.Router()
-const experienceList = []
+const EXPERIENCES_LIST_FILE_PATH = './data/experiences_list.json'
+const experiencesList = []
 
 
 const __filename = fileURLToPath(import.meta.url)
@@ -19,8 +20,44 @@ function loggerMiddleware(req,res,next){
   console.log('New Request = ',req.method,req.url)
 }
 
-function saveExperience(){
+async function addNewExperience(){
+  try{
+    const newExperience = {
+      id:experiencesList.length,
+      subject:' ',
+      Email: ' ',
+      FullName : ' ',
+      Description : ' ',
+      isActive : false,
+      creationDate: ' ',
+      updatedDate: ' '
+    }
 
+    experiencesList.push(newExperience)
+    saveExperience()
+    
+  }catch(err){
+    throw err
+  }
+}
+
+async function loadExperience(){
+  try{
+    const experiencesListJSON = await fs.readFile(EXPERIENCES_LIST_FILE_PATH,'utf-8')
+    experiencesList.push(JSON.parse(...experiencesListJSON))
+  }catch(err){
+     throw err
+  }
+}
+
+async function saveExperience(){
+  try{
+    const experienceJSON = JSON.stringify(experiencesList)
+    await fs.writeFile(EXPERIENCES_LIST_FILE_PATH,'utf-8')
+
+  }catch(err){
+    throw err
+  }
 }
 
 router.get('/test',(req,res)=>{
