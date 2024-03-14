c83ef946325a753757a48a91c127c52cae14d4ed
Mohammad Hassan Amrollahi <mh.amrollahi@gmail.com>
2024-03-14 14:48:43 +0330

    add logger Middleware to index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import fs from 'fs/promises'
 
 const app = express()
 const port = 3000
 const router = express.Router()
+const experienceList = []
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -12,12 +15,22 @@ const __dirname = path.dirname(__filename)
 console.log('dirName = ',__dirname)
 console.log('fileName = ',__filename)
 
-router.get('/',(req,res)=>{
+function loggerMiddleware(req,res,next){
+  console.log('New Request = ',req.method,req.url)
+}
+
+function saveExperience(){
+
+}
+
+router.get('/test',(req,res)=>{
   res.sendFile(path.join(__dirname + '/myExperience.html'))
   // res.sendFile('myExperience.html')
 })
 
 app.use(router)
+app.use(loggerMiddleware)
+
 app.use(express.static(__dirname));
 
 app.listen(port,()=>{
