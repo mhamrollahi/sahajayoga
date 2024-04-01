52a54711867d6842f3d3c6e57f6c58370a1c3ae4
mh Amrollahi <mh.amrollahi@gmail.com>
2024-04-01 21:32:10 +0330

    add aboutKundalini.html and css and add 2 folder for backend

--- public/assets/JS/src/services.js
+++ public/assets/JS/src/services.js
@@ -1,22 +1,22 @@
+import { log } from "console";
 import fs from "fs/promises";
 
-const EXPERIENCES_LIST_FILE_PATH = './public/data/experiences_list.json'
-
+const EXPERIENCES_LIST_FILE_PATH = "./public/data/experiences_list.json";
 
 export function generateNewExperienceID(experiencesList) {
   const lastExperience = experiencesList[experiencesList.length - 1];
-  
-  console.log('lastExperience = ', lastExperience)
-  
+
+  console.log("lastExperience = ", lastExperience);
+
   const id = lastExperience ? lastExperience.id + 1 : 1;
-  console.log(`id = ${id}`)
+  console.log(`id = ${id}`);
   return id;
 }
 
-export function addNewExperience(_experienceRec,experiencesList) {
+export function addNewExperience(_experienceRec, experiencesList) {
   const id = generateNewExperienceID(experiencesList);
 
-  try { 
+  try {
     const newRecord = {
       id: id,
       subject: _experienceRec.txtSubject,
@@ -37,23 +37,25 @@ export function addNewExperience(_experienceRec,experiencesList) {
   }
 }
 
-export async function saveExperience(experiencesList){
+export async function saveExperience(experiencesList) {
   try {
     const experienceJSON = JSON.stringify(experiencesList);
-    console.log('save Experience ... experienceJSON =',experienceJSON)
+    console.log("save Experience ... experienceJSON =", experienceJSON);
 
     await fs.writeFile(EXPERIENCES_LIST_FILE_PATH, experienceJSON);
-
   } catch (err) {
     throw err;
   }
 }
 
-export async function loadExperience(){
-  try{
-    const experienceJSON = await fs.readFile(EXPERIENCES_LIST_FILE_PATH,'utf-8')
-    return JSON.parse(experienceJSON)
-  }catch(err){
-    return err
+export async function loadExperience() {
+  try {
+    const experienceJSON = await fs.readFile(
+      EXPERIENCES_LIST_FILE_PATH,
+      "utf-8"
+    );
+    return JSON.parse(experienceJSON);
+  } catch (err) {
+    return err;
   }
 }
