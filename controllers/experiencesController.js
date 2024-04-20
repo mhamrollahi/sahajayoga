774aad630c2dd10a6ef6cbd529108992f86c1a0c
Mohammad Hassan Amrollahi <mh.amrollahi@gmail.com>
2024-04-20 16:31:03 +0330

    edit model and controller for experience

--- controllers/experiencesController.js
+++ controllers/experiencesController.js
@@ -1,19 +1,29 @@
-import experienceModel from "../models/experienceModel.js";
+// import {testData,index} from "../models/experienceModel.js";
+import {index,testData} from "../models/experienceModel.js";
 
+console.log('in experience Controllers file ...');
 class experienceController {
-  index(req, res) {
-    // res.send({message:'list function from experience controllers ....'})
-    const allExperiences = experienceModel.list();
-    res.send(allExperiences)
+ async index(req, res) {
+    
+     // res.send({message:'list function from experience controllers ....'})
+    const allExperiences = await index()
+    console.log('in controller = one recorde =>>',allExperiences[0][3])
+    
+    const {fullName,email,isActive,Title,Description} = allExperiences[0][0]
+    console.log('in controller = ',fullName,email,Title,Description)
+    res.send({fullName,email,isActive,Title,Description})
+    res.end()
     // res.render("experience/index", { experiences: allExperiences });
   }
 
   test(req, res) {
     console.log('in experience Controller ...')
+    index()
+
     res.send({
       success: true,
       message: "from experience Controller test Route .... Damam",
-      Damet: "Gharm ...",
+      Damet: testData,
     });
   }
 }
--- models/experienceModel.js
+++ models/experienceModel.js
@@ -1,14 +1,39 @@
 import myDatabase from '../configs/dbConfig.js'
 
+console.log('in experience Model file ...')
 
-function list(){
-  console.log('in list function in experience Model ...')
-  // const _list = await myDatabase.query('SELECT * FROM experiences')
-  // console.log(_list)
-  // return _list
+export async function index(){
+
+  // console.log('in index function in experience Model ...')
+  
+  const _list = await myDatabase.query('SELECT * FROM experiences')
+  // console.log('in index function in experience Model ...',_list)
+  return _list
+}
+
+export async function insertExperiece(){
+  try {
+
+  } catch (error) {
+    
+  }
+
+}
+
+export async function deleteExperience(){
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+export async function editExperience(){
+  try {
+    
+  } catch (error) {
+    
+  }
 }
 
 
-
-
-export default list
